b9ac93464c42d71d9b70e1dcba1c7c38c910b051
Charles. G. Jovans <chuckjovans@gmail.com>
2023-04-12 14:26:28 +0300

    fourth commit

--- config.py
+++
@@ -1,2 +0,0 @@
-AUTHY_API_KEY = "dpU6FwOcrwJsEOMZ0KE2LpvsDWuiHqT1"
-SECRET_KEY = "restaurant"
--- run.py
+++
@@ -1,8 +0,0 @@
-from restaurant.routes import app
-
-#checks if main.py has executed directly and not imported
-if __name__ == '__main__':
-    app.run(debug = True)
-
-
-
